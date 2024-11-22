319002313bc42f8d7060e200a82f6ab20a509dc4
Griffenholm <nicolinedh@gmail.com>
2024-11-22 10:23:59 +0100

    TextUI med metoder

---
+++ src/TextUI.java
@@ -0,0 +1,31 @@
+package src;
+
+import java.util.Scanner;
+
+public class TextUI {
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void displayMSG(String msg){
+        System.out.println(msg);
+    }
+
+    public static String promptText(String msg){
+        System.out.println(msg); //Stil brugeren et spørgsmål
+        String input = scan.nextLine();
+        return input;
+    }
+
+    public static int promptNumeric(String msg){
+        System.out.println(msg); //Stil brugeren et spørgsmål
+        String input = scan.nextLine();
+        int number;
+        try {
+            number = Integer.parseInt(input);
+        } catch (NumberFormatException e){
+            displayMSG("Please type a number");
+            number = promptNumeric(msg);
+        }
+        return number;
+
+    }
+}
